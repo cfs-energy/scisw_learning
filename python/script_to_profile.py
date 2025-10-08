d0b1f5ee0f721732a82ee4c8d6bd41337bcf79b2
James Logan <118752918+jlogan-cfs@users.noreply.github.com>
2025-10-08 11:33:45 -0400

    Add zip and pyinstrument (#4)
    ## 2025-10-08
    
    * Add zip archives to file I/O section
    * Add pyinstrument to performance section
    * Update cron schedule to run tests once per week instead of daily
    * Update python version to 3.11+

--- python/script_to_profile.py
+++ python/script_to_profile.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 def do_stuff():
     a = np.random.uniform(-1.0, 1.0, 1000)
     b = np.outer(a, a)
-    c = np.dot(b, b)
-    d = np.dot(b, a)
+    _c = np.dot(b, b)
+    _d = np.dot(b, a)
 
     plt.imshow(b)
     plt.show(block=False)
